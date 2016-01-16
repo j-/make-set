dcaee11a4ce39069b8138827220130fb4f0dceab
j- <j@skeoh.com>
2016-01-16 18:41:52 +1100

    Use `function ()` syntax
    Allows tests to run on older nodejs versions.

--- test/make-set.js
+++ test/make-set.js
@@ -1,28 +1,28 @@
 const makeSet = require('../dist/make-set');
 const assert = require('assert');
 
-describe('makeSet', () => {
-	it('is a function', () => {
+describe('makeSet', function () {
+	it('is a function', function () {
 		assert.equal(typeof makeSet, 'function');
 	});
 
-	it('returns the input if already a Set', () => {
+	it('returns the input if already a Set', function () {
 		const input = new Set([1, 2, 3]);
 		const output = makeSet(input);
 		assert.equal(input, output);
 	});
 
-	it('can be called without an argument', () => {
+	it('can be called without an argument', function () {
 		const set = makeSet();
 		assert.ok(set instanceof Set);
 	});
 
-	it('can be called with an array', () => {
+	it('can be called with an array', function () {
 		const set = makeSet([1, 2, 3]);
 		assert.ok(set instanceof Set);
 	});
 
-	it('contains all array items', () => {
+	it('contains all array items', function () {
 		const set = makeSet(['a', 'b', 'c']);
 		const iter = set.values();
 		assert.equal(iter.next().value, 'a');
